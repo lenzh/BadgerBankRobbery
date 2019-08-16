d3624e476ba46a2131a06efab49d384e93ef12c6
Lenzh <lorenzo0109@hotmail.nl>
2019-08-16 17:19:56 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,24 @@
+
+-------------------------
+------ ESX SUPPORT ------
+-------------------------
+
+ESX = nil
+local PlayerData                = {}
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(10)
+    end
+
+    PlayerData = ESX.GetPlayerData()
+end)
+
 -------------------------
 --- BadgerBankRobbery ---
 -------------------------
